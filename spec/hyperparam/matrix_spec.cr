99ec3dfc93edbed010fec9d583c26b4b174a348d
Bart Blast <bart@bartblast.com>
2019-04-11 18:28:19 +0200

    Implement matrix multiplication, matrix indexable access and some matrix utility methods

--- spec/hyperparam/matrix_spec.cr
+++ spec/hyperparam/matrix_spec.cr
@@ -5,24 +5,103 @@ module Hyperparam
   describe Matrix do
     describe "initialization" do
       it "supports creation with a linear index initialiser" do
-        matrix = Matrix(Int32, 2, 3).new { |idx| idx }
+        a = Matrix(Int32, 2, 3).new { |idx| idx }
         # TODO: test values
       end
 
       it "supports creation with an indices initialiser" do
-        Matrix(UInt32, 2, 3).build { |i, j| i * j }
+        a = Matrix(UInt32, 2, 3).build { |i, j| i * j }
+        # TODO: test values
+      end 
+
+      it "supports building from a StaticArray" do
+         a = Matrix(Int32, 2, 3).from StaticArray[1, 2, 3, 4, 5, 6]
+        # TODO: test values
       end      
 
       it "supports creation with a single, repeated value" do
-        matrix = Matrix(Int32, 2, 3).of 123
+        a = Matrix(Int32, 2, 3).of 123
         # TODO: test values
       end      
     end
 
+    describe "#==" do
+      it "returns false for differently sized matrices" do
+        a = Matrix(Nil, 2, 3).of nil
+        b = Matrix(Nil, 3, 4).of nil
+        (a == b).should eq(false)
+      end
+
+      it "uses element equality for equally sized matrices" do
+        a = Matrix(Int32, 2, 3).of 42
+        b = Matrix(Float32, 2, 3).of 42.0
+        (a == b).should eq(true)
+      end
+
+      it "return false for other objects" do
+        a = Matrix(Nil, 2, 3).of nil
+        (a == "Foo").should eq(false)
+      end
+    end    
+
+    describe "#*" do
+      it "supports matrix multiplication" do
+        a = Matrix(Int32, 2, 3).new { |i| i }
+        b = Matrix(Int32, 3, 2).new { |i| i }
+        expected = Matrix(Int32, 2, 2).from StaticArray[10, 13, 28, 40]
+        (a * b).should eq(expected)
+      end
+    end
+
+    describe "#[]" do
+      a = Matrix(Int32, 3, 4).new { |idx| idx }
+
+      it "supports retrieval based on row, column addresses" do
+        a[0, 0].should eq(0)
+        a[0, 3].should eq(3)
+        a[1, 0].should eq(4)
+      end
+
+      it "supports negative index lookups" do
+        a[-1, -1].should eq(a[2, 3])
+      end
+
+      it "raises an index error for invalid indices" do
+        expect_raises(IndexError) { a[10, 0] }
+        expect_raises(IndexError) { a[-10, 0] }
+      end
+    end
+
+    describe "#col" do
+      a = Matrix(Int32, 2, 3).new { |idx| idx }
+      a.col(0).should eq(StaticArray[0, 3])
+      a.col(1).should eq(StaticArray[1, 4])
+    end
+
+    describe "#num_cols" do
+      it "provides the column count" do
+        a = Matrix(Nil, 2, 3).of nil
+        a.num_cols.should eq(3)
+      end
+    end  
+    
+    describe "#num_rows" do
+      it "provides the row count" do
+        a = Matrix(Nil, 2, 3).of nil
+        a.num_rows.should eq(2)
+      end
+    end
+
+    describe "#row" do
+      a = Matrix(Int32, 2, 3).new { |idx| idx }
+      a.row(0).should eq(StaticArray[0, 1, 2])
+      a.row(1).should eq(StaticArray[3, 4, 5])
+    end
+
     describe "#size" do
       it "provides the total capacity" do
-        matrix = Matrix(Nil, 2, 3).of nil
-        matrix.size.should eq(6)
+        a = Matrix(Nil, 2, 3).of nil
+        a.size.should eq(6)
       end
     end
   end
--- src/hyperparam/matrix.cr
+++ src/hyperparam/matrix.cr
@@ -9,6 +9,7 @@
 # Based on: https://github.com/aca-labs/matrix
 module Hyperparam
   struct Matrix(T, M, N)
+    include Indexable(T)
 
     # Creates Matrix, yielding the linear index for each element to provide an
     # initial value.
@@ -26,14 +27,116 @@ module Hyperparam
       end
     end
 
+    # Creates a Matrix from elements contained within a StaticArray.
+    #
+    # The matrix will be filled rows first, such that an array of
+    #
+    #   [1, 2, 3, 4]
+    #
+    # becomes
+    #
+    #   | 1  2 |
+    #   | 3  4 |
+    #
+    def self.from(list : StaticArray(T, A))
+      {{ raise("Not enough elements to fill matrix") if A < M * N }}
+
+      Matrix(T, M, N).new do |idx|
+        list[idx]
+      end
+    end
+
     # Creates a Matrix with each element initialized as *value*.
     def self.of(value : T)
       Matrix(T, M, N).new { value }
+    end
+
+    # Equality. Returns `true` if each element in `self` is equal to each
+    # corresponding element in *other*.
+    def ==(other : Matrix(U, A, B)) forall U
+      {% if A == M && B == N %}
+        each_with_index do |e, i|
+          return false unless e == other[i]
+        end
+        true
+      {% else %}
+        false
+      {% end %}
+    end
+
+    # Equality with another object, or differently sized matrix. Always `false`.
+    def ==(other)
+      false
     end    
+
+    # Performs a matrix multiplication with *other*.
+    def *(other : Matrix(_, A, B))
+      {{ raise("Dimension mismatch, cannot multiply a #{M}x#{N} by a #{A}x#{B}") \
+        unless N == A }}
+
+      Matrix(typeof(self[0] * other[0]), M, B).build do |i, j|
+        pairs = row(i).zip other.col(j)
+        pairs.map(&.product).sum
+      end
+    end
+    
+    # Retrieves the value of the element at *i*,*j*.
+    #
+    # Indicies are zero-based. Negative values may be passed for *i* and *j* to
+    # enable reverse indexing such that `self[-1, -1] == self[M - 1, N - 1]`
+    # (same behaviour as arrays).
+    def [](i : Int, j : Int) : T
+      to_unsafe[index(i, j)]
+    end
+    
+    # Gets the contents of column *j*.
+    def col(j : Int)
+      StaticArray(T, M).new { |i| self[i, j] }
+    end
+
+    # Count of columns.
+    def num_cols
+      N
+    end
+
+    # Count of rows.
+    def num_rows
+      M
+    end     
+
+    # Gets the contents of row *i*.
+    def row(i : Int)
+      StaticArray(T, N).new { |j| self[i, j] }
+    end   
 
     # Gets the capacity (total number of elements) of `self`.
     def size
       M * N
     end
+
+    # Map *i*,*j* coords to an index within the buffer.
+    protected def index(i : Int, j : Int)
+      i += num_rows if i < 0
+      j += num_cols if j < 0
+
+      raise IndexError.new if i < 0 || j < 0
+      raise IndexError.new unless i < num_rows && j < num_cols
+
+      i * N + j
+    end
+
+    # Returns the pointer to the underlying element data.
+    protected def to_unsafe : Pointer(T)
+      @buffer
+    end
+
+    # Returns the element at the given linear index, without doing any bounds
+    # check.
+    #
+    # Used by `Indexable`
+    @[AlwaysInline]
+    protected def unsafe_fetch(index : Int)
+      to_unsafe[index]
+    end    
   end
 end
